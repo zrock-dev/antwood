13dedbaea095e165993fc30b0ac57fc5aa158c2c
luiggy_mamani <luiggy.mamani.12777190@usalesiana.edu.bo>
2023-11-06 15:51:26 -0400

    ci: model to get sneaker joined

---
+++ backend/product_management/app/models/SneakerJoined.go
@@ -0,0 +1,17 @@
+package models
+
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
+type SneakerWithColors struct {
+	ID            primitive.ObjectID   `json:"_id,omitempty"`
+	Name          string               `json:"name"`
+	Description   string               `json:"description"`
+	Price         int                  `json:"price"`
+	Colors        []SneakerColor       `json:"colors"`
+	Tags          []string             `json:"tags"`
+	Reviews       []primitive.ObjectID `json:"reviews"`
+	Qualification int                  `json:"qualification"`
+	LastDate      primitive.DateTime   `json:"lastDate"`
+	SalesQuantity int                  `json:"salesQuantity"`
+	PromotionCode primitive.ObjectID   `json:"promotionCode"`
+}
