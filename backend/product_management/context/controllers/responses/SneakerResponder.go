f9148fe1a2eab30941e07dec2ca4e6e08ddd6d0e
luiggy_mamani <luiggy.mamani.12777190@usalesiana.edu.bo>
2023-11-06 15:51:58 -0400

    feat: send sneakers joined with their first color with pagination

--- backend/product_management/context/controllers/responses/SneakerResponder.go
+++ backend/product_management/context/controllers/responses/SneakerResponder.go
@@ -1,5 +1,57 @@
 package responses
 
-func SendSneakersByPagination() {
-	// need to implemented the method
+import (
+	"context"
+	"strconv"
+
+	"product_management/app/database"
+	"product_management/app/models"
+
+	"github.com/gofiber/fiber/v2"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+func SendSneakersByPagination(c *fiber.Ctx) error {
+	page := c.Query("page", "1")
+	pageSize := c.Query("pageSize", "9")
+	pageInt, _ := strconv.Atoi(page)
+	pageSizeInt, _ := strconv.Atoi(pageSize)
+
+	skip := (pageInt - 1) * pageSizeInt
+	limit := pageSizeInt
+
+	pipeline := mongo.Pipeline{
+		bson.D{
+			{Key: "$lookup", Value: bson.D{
+				{Key: "from", Value: "sneakerColors"},
+				{Key: "localField", Value: "colors.0"},
+				{Key: "foreignField", Value: "_id"},
+				{Key: "as", Value: "colors"},
+			}},
+		},
+		bson.D{
+			{Key: "$skip", Value: skip},
+		},
+		bson.D{
+			{Key: "$limit", Value: limit},
+		},
+	}
+
+	cursor, err := database.SneakerCollection.Aggregate(context.TODO(), pipeline)
+	if err != nil {
+		return c.Status(500).JSON(fiber.Map{"error": err.Error()})
+	}
+	defer cursor.Close(context.TODO())
+
+	var sneakersWithColors []models.SneakerWithColors
+	for cursor.Next(context.TODO()) {
+		var sneakerWithColors models.SneakerWithColors
+		if err := cursor.Decode(&sneakerWithColors); err != nil {
+			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": err.Error()})
+		}
+		sneakersWithColors = append(sneakersWithColors, sneakerWithColors)
+	}
+
+	return c.JSON(sneakersWithColors)
 }
