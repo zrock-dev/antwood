b0b25c426118802b5f6a2d4e21c795af133bd891
VictorCespedesCC <victor.cespedes.9374448@usalesiana.edu.bo>
2023-11-09 11:34:18 -0400

    refactor

--- frontend/src/components/product/RelatedProducts/RelatedProducts.jsx
+++ frontend/src/components/product/RelatedProducts/RelatedProducts.jsx
@@ -3,7 +3,7 @@ import ProductCard from './ProductCard';
 
 const RelatedProducts = ({ relatedProducts }) => {
   return (
-    <div className="related-products-container">
+    <div className="related-products-scroll">
       {relatedProducts.map((product) => (
         <ProductCard key={product._id} product={product} />
       ))}
