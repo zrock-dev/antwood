06a3b611d8690f985099369e10a517363fb5fabe
VictorCespedesCC <victor.cespedes.9374448@usalesiana.edu.bo>
2023-11-09 09:07:02 -0400

    request

--- frontend/src/requests/SneakersRequest.js
+++ frontend/src/requests/SneakersRequest.js
@@ -10,3 +10,13 @@ export const getAllProductsByPagination = async (page) => {
 	const response = await axios.get(`${SERVER_DOMAIN}/sneakers?page=${page}`);
 	return response.data;
 };
+
+export const getSneakersByFilters = async (filters) => {
+  try {
+    const response = await axios.post(`${SERVER_DOMAIN}/sneakers/filter`, filters);
+    return response.data;
+  } catch (error) {
+    console.error('Error when obtain sneakers by filters:', error);
+    throw error;
+  }
+};
