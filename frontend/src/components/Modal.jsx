6668a206efd696008f3e33f2233e2a3c786c935f
Daniel Espinoza <daniel.espinoza.8812451@usalesiana.edu.bo>
2023-11-10 23:58:57 -0400

    fix: styles for large widnows

--- frontend/src/components/Modal.jsx
+++ frontend/src/components/Modal.jsx
@@ -11,9 +11,9 @@ function Modal({
 
   useEffect(()=>{
     if(isModalOpen){
-      document.body.style.overflowY = "hidden";
+      document.body.style.overflow = "hidden";
     }else{
-      document.body.style.overflowY = "auto";
+      document.body.style.overflow = "auto";
     }
   },[isModalOpen])
 
