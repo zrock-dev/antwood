350ba8d847ffc78808cbfa966bcf1404cb54bf00
Santiago C. Saavedra <santisaavcon+commits@gmail.com>
2023-11-13 09:48:25 -0400

    chore: nginx reverse proxy implementation
    Created a ngix configuration file (solestyle.conf) to allow the
    communication of the services with the frontend.
    
    Added the web service to the docker_compose template
    
    Removed AUTH_DOMAIN and SERVER_DOMAIN vars from the frontend

--- frontend/src/requests/AuthRequest.js
+++ frontend/src/requests/AuthRequest.js
@@ -1,9 +1,6 @@
 import axios from "axios";
 
 
-
-import { AUTH_DOMAIN } from "./Requester";
-
 const axiosConfig = {
   headers: {
     "Content-Type": "application/json",
@@ -14,36 +11,37 @@ const axiosConfig = {
 
 
 
-export const loginUser =(user, provider) => axios.post(`${AUTH_DOMAIN}/auth/login?provider=${provider}`,user,axiosConfig)
+export const loginUser =(user, provider) => axios.post(`/auth/login?provider=${provider}`,user,axiosConfig)
 
 export const registerUser = (user,provider) =>
-  axios.post(`${AUTH_DOMAIN}/auth/register?provider=${provider}`, user, axiosConfig);
+  axios.post(`/auth/register?provider=${provider}`, user, axiosConfig);
 
 
 
 export const getUser = async () =>{
-  const res = await axios.get(`${AUTH_DOMAIN}/auth/user`, axiosConfig)
+  const res = await axios.get(`/auth/user`, axiosConfig)
   return res.data
 }
   
 
+
 export const getUserByEmail = (email) =>{
-  return axios.get(`${AUTH_DOMAIN}/auth/user/${email}`, axiosConfig);
+  return axios.get(`/auth/user/${email}`, axiosConfig);
 
 }
   
 
 
 export const logoutUser = () =>
-  axios.get(`${AUTH_DOMAIN}/auth/logout`, axiosConfig);
+  axios.get(`/auth/logout`, axiosConfig);
 
 export const getCodeToVerifyAccount = async (email) => {
-  const response = await axios.get(`${AUTH_DOMAIN}/auth/verify-account/${email}`, axiosConfig)
+  const response = await axios.get(`/auth/verify-account/${email}`, axiosConfig)
   return  response.data
 }
 
 
 export const isValidCode = async (code, encryptedCode) =>{
-  const response = await axios.get(`${AUTH_DOMAIN}/auth/verify-code?encryptedcode=${encryptedCode}&code=${code}`, axiosConfig);
+  const response = await axios.get(`/auth/verify-code?encryptedcode=${encryptedCode}&code=${code}`, axiosConfig);
   return response.data && response.data.status
 }
--- frontend/src/requests/ReviewRequest.js
+++ frontend/src/requests/ReviewRequest.js
@@ -1,5 +1,4 @@
 import axios from 'axios';
-import { SERVER_DOMAIN } from './Requester';
 import Review from '@/components/reviews/Review';
 
 const axiosConfig = {
@@ -10,16 +9,16 @@ const axiosConfig = {
   };
   
 export const addReview = async (sneakerId, review) => {
-	const response = await axios.post(`${SERVER_DOMAIN}/review/${sneakerId}`,review,axiosConfig);
+	const response = await axios.post(`/review/${sneakerId}`,review,axiosConfig);
 	return response.data;
 };
 
 export const getReviewsByUserEmail = async (sneakerId, userEmail) => {
-  const response = await axios.get(`${SERVER_DOMAIN}/review/user/${sneakerId}/${userEmail}`,axiosConfig);
+  const response = await axios.get(`/review/user/${sneakerId}/${userEmail}`,axiosConfig);
   return response.data;
 }
 
 export const getReviewsByAmount = async (sneakerId, from , amount) => {
-  const response = await axios.get(`${SERVER_DOMAIN}/review/${sneakerId}?index=${from}&amount=${amount}`,axiosConfig);
+  const response = await axios.get(`/review/${sneakerId}?index=${from}&amount=${amount}`,axiosConfig);
   return response.data;
 }
