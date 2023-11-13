55da09c8616b7a17f8660eb82fcc0d9fa08e6121
Santiago C. Saavedra <santisaavcon+commits@gmail.com>
2023-11-13 09:48:25 -0400

    chore: nginx reverse proxy implementation
    Created a ngix configuration file (solestyle.conf) to allow the
    communication of the services with the frontend.
    
    Added the web service to the docker_compose template
    
    Removed AUTH_DOMAIN and SERVER_DOMAIN vars from the frontend

--- frontend/src/requests/AuthRequest.js
+++ frontend/src/requests/AuthRequest.js
@@ -1,9 +1,6 @@
 import axios from "axios";
 
 
-
-import { AUTH_DOMAIN } from "./Requester";
-
 const axiosConfig = {
   headers: {
     "Content-Type": "application/json",
@@ -14,32 +11,32 @@ const axiosConfig = {
 
 
 
-export const loginUser =(user, provider) => axios.post(`${AUTH_DOMAIN}/auth/login?provider=${provider}`,user,axiosConfig)
+export const loginUser =(user, provider) => axios.post(`/auth/login?provider=${provider}`,user,axiosConfig)
 
 export const registerUser = (user,provider) =>
-  axios.post(`${AUTH_DOMAIN}/auth/register?provider=${provider}`, user, axiosConfig);
+  axios.post(`/auth/register?provider=${provider}`, user, axiosConfig);
 
 
 
 export const getUser = () =>
-  axios.get(`${AUTH_DOMAIN}/auth/user?provider=${provider}`, axiosConfig);
+  axios.get(`/auth/user?provider=${provider}`, axiosConfig);
 
 
 export const getUserByEmail = (email) =>
-  axios.get(`${AUTH_DOMAIN}/auth/user/${email}`, axiosConfig);
+  axios.get(`/auth/user/${email}`, axiosConfig);
 
 
 
 export const logoutUser = (provider) =>
-  axios.post(`${AUTH_DOMAIN}/auth/logout?provider=${provider}`, axiosConfig);
+  axios.post(`/auth/logout?provider=${provider}`, axiosConfig);
 
 export const getCodeToVerifyAccount = async (email) => {
-  const response = await axios.get(`${AUTH_DOMAIN}/auth/verify-account/${email}`, axiosConfig)
+  const response = await axios.get(`/auth/verify-account/${email}`, axiosConfig)
   return  response.data
 }
 
 
 export const isValidCode = async (code, encryptedCode) =>{
-  const response = await axios.get(`${AUTH_DOMAIN}/auth/verify-code?encryptedcode=${encryptedCode}&code=${code}`, axiosConfig);
+  const response = await axios.get(`/auth/verify-code?encryptedcode=${encryptedCode}&code=${code}`, axiosConfig);
   return response.data && response.data.status
 }
