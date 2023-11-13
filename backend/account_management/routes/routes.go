303780dd30a9e453ba08a619a9cdb8146d036419
Daniel Espinoza <daniel.espinoza.8812451@usalesiana.edu.bo>
2023-11-13 15:58:15 -0400

    authentication flow backend

--- backend/account_management/routes/routes.go
+++ backend/account_management/routes/routes.go
@@ -8,8 +8,11 @@ import (
 func AuthRoutes(app *fiber.App) {
 	app.Post("/auth/register", controllers.Register)
 	app.Post("/auth/login", controllers.Login)
+
+
+
+	app.Get("/auth/logout", controllers.Logout)
 	app.Get("/auth/user", controllers.GetUserByToken)
-	app.Post("/auth/logout", controllers.Logout)
 	app.Get("/auth/user/:email", controllers.GetUserByEmail)
 	app.Get("/auth/verify-account/:email", controllers.VerifyAccount)
 	app.Get("/auth/verify-code", controllers.VerifyCode)
