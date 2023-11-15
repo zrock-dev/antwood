b2dd6ac328b5b9a1b3b11a91fda1a95e1b44b22d
VictorCespedesCC <victor.cespedes.9374448@usalesiana.edu.bo>
2023-11-15 18:37:32 -0400

    change icon fix

--- frontend/src/components/shopping_Cart/Popup.jsx
+++ frontend/src/components/shopping_Cart/Popup.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import styled from '@emotion/styled'; 
 import '../../styles/shopping_cart/Popup.css';
 import IconButton from '@material-ui/core/IconButton';
-import CancelIcon from '@mui/icons-material/Cancel';
+import CancelIcon from '@material-ui/icons/Cancel';
 
 const PopupMain = styled.section`
   position: fixed;
