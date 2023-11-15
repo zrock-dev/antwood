d2743d1cfe111ba1f7e36f07485934a3ae15ab80
VictorCespedesCC <victor.cespedes.9374448@usalesiana.edu.bo>
2023-11-15 09:41:30 -0400

    fix low error

--- frontend/src/components/product/RelatedProducts/RelatedProductsSection.jsx
+++ frontend/src/components/product/RelatedProducts/RelatedProductsSection.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import ProductCard from './ProductCard';
+import ProductCard from '../../products/ProductCard';
 
 
 const RelatedProductsSection = ({ relatedProducts }) => {
