4a55cfb2bad3ed6bb0ac2831ffadfa5a22d6e555
luiggy_mamani <luiggy.mamani.12777190@usalesiana.edu.bo>
2023-11-19 14:36:23 -0400

    feat: render suggestions on the frontend when the input change

--- frontend/src/components/search/Searcher.jsx
+++ frontend/src/components/search/Searcher.jsx
@@ -3,25 +3,47 @@
 import { getSneakerSearchSuggestions } from '@/requests/SneakersRequest';
 import { useEffect, useState } from 'react';
 
+import '../../styles/search/search.css';
+import SearchIcon from '@/icons/SearchIcon';
+
 const Searcher = () => {
 	const [input, setInput] = useState('');
+	const [suggestions, setSuggestions] = useState([]);
 
 	useEffect(() => {
-		if (input !== '') {
+		if (input.trim() !== '') {
 			getSneakerSearchSuggestions(input)
-				.then((response) => console.log(response))
-				.catch('error getting suggestions');
+				.then((data) => setSuggestions(data.names))
+				.catch((e) => console.log(e));
+		} else {
+			setSuggestions([]);
 		}
 	}, [input]);
+
 	return (
-		<div>
-			<input
-				type="text"
-				placeholder="Search"
-				value={input}
-				onChange={(e) => setInput(e.target.value)}
-			/>
-			<button>Search</button>
+		<div className="search-main-container">
+			<div className="search-searcher-container">
+				<input
+					type="text"
+					placeholder="Search"
+					value={input}
+					onChange={(e) => setInput(e.target.value)}
+					autoComplete="false"
+				/>
+				<button title="search">
+					<SearchIcon />
+				</button>
+			</div>
+			{suggestions?.length > 0 && (
+				<div className="search-suggestions-container">
+					{suggestions.map((suggestion, index) => (
+						<button className="search-suggestion" name={suggestion} key={index}>
+							<SearchIcon />
+							{suggestion}
+						</button>
+					))}
+				</div>
+			)}
 		</div>
 	);
 };
