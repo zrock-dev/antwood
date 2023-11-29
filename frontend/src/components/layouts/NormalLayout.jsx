a256732254025d7fd530623153d921d957aa4890
luiggy_mamani <luiggy.mamani.12777190@usalesiana.edu.bo>
2023-11-29 17:18:31 -0400

    fix: move filter provider to general layout to clear filters when is using searh on the admin panel

--- frontend/src/components/layouts/NormalLayout.jsx
+++ frontend/src/components/layouts/NormalLayout.jsx
@@ -1,17 +1,14 @@
 import CartProvider from '@/context/CartContext';
 import Navbar from '../navbar/Navbar';
 import Layout from './Layout';
-import ProductResultsProvider from '@/context/ProductResultsContext';
 
 const NormalLayout = ({ children }) => {
 	return (
 		<Layout>
 			<CartProvider>
-				<ProductResultsProvider>
-					<Navbar />
-					<div id="overlay"></div>
-					{children}
-				</ProductResultsProvider>
+				<Navbar />
+				<div id="overlay"></div>
+				{children}
 			</CartProvider>
 		</Layout>
 	);
