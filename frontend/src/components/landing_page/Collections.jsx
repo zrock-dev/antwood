1f1ff018680d748267a2e5b97801ed5ed6284338
luiggy_mamani <luiggy.mamani.12777190@usalesiana.edu.bo>
2023-11-30 15:37:02 -0400

    feat: redirect to collections

--- frontend/src/components/landing_page/Collections.jsx
+++ frontend/src/components/landing_page/Collections.jsx
@@ -1,9 +1,17 @@
+'use client';
+import { useContext } from 'react';
 import '../../styles/landing_page/collections.css';
+import { ProductResultsContext } from '@/context/ProductResultsContext';
 
 const Collections = () => {
+	const { addTag } = useContext(ProductResultsContext);
+
 	return (
 		<div className="landing-collections-main-container">
-			<button className="langin-collection-container">
+			<button
+				className="langin-collection-container"
+				onClick={() => addTag('men')}
+			>
 				<img
 					className="langin-collection-image"
 					src="https://res.cloudinary.com/dex16gvvy/image/upload/v1701348624/solestyle/maqxnlwkfmhsmma1otuw.jpg"
@@ -11,7 +19,10 @@ const Collections = () => {
 				/>
 				<h3 className="langin-collection-title">MEN</h3>
 			</button>
-			<button className="langin-collection-container">
+			<button
+				className="langin-collection-container"
+				onClick={() => addTag('women')}
+			>
 				<img
 					className="langin-collection-image"
 					src="https://res.cloudinary.com/dex16gvvy/image/upload/v1701348624/solestyle/kef5mxgc1vj843hbg5hj.jpg"
@@ -19,7 +30,10 @@ const Collections = () => {
 				/>
 				<h3 className="langin-collection-title">WOMEN</h3>
 			</button>
-			<button className="langin-collection-container">
+			<button
+				className="langin-collection-container"
+				onClick={() => addTag('kids')}
+			>
 				<img
 					className="langin-collection-image"
 					src="https://res.cloudinary.com/dex16gvvy/image/upload/v1701348624/solestyle/gx4s1gz6xatskz1khdx5.jpg"
