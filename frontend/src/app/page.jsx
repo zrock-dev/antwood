8bd531f90a483178a9dec02d8e2c94b4c875f52a
luiggy_mamani <luiggy.mamani.12777190@usalesiana.edu.bo>
2023-11-30 15:02:05 -0400

    feat: add front page to the landing page

--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -2,11 +2,13 @@
 import ProductSuggestions from '@/components/products/product_renderers/ProductSuggestions';
 import '../styles/landing_page/page.css';
 import NormalLayout from '@/components/layouts/NormalLayout';
+import FrontPage from '@/components/landing_page/FrontPage';
 
 export default function LandingPage() {
 	return (
 		<NormalLayout>
 			<main className="landing-page-main-container">
+				<FrontPage />
 				<span className="vertical-separator"></span>
 				<span className="subtitle">Our suggestions for you</span>
 				<ProductSuggestions />
